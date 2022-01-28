353a5c32ded786ab041b600fe2760bd1df15b6a0
stephen talley <stvlley.amzn@gmail.com>
2022-01-28 14:07:20 -0600

    commit tree 9223061d3154ae341f8a58df9719a0bce24c218d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+//define companyName to equal "Scuber"
+//is a const
+const companyName = 'Scuber';
+
+//use let to declare mostProfitableNeighborhood to equal "Chelsea"
+let mostProfitableNeighborhood = "Chelsea";
+
+//companyCeo to equal "Susan Smith" usinf let
+let companyCeo = "Susan Smith";
